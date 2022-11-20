f37daa70a0aa152f5789859e3cb5206ee297d5e7
Chris <Whisperswild@gmail.com>
2022-11-20 11:21:51 -0600

    Fix font links. Build password match logic in javascript.

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//add classes (.wrong) to: class="form-element password" 
-//remove class .hidden from .wrong-password
+
+
+const pwform = document.querySelector('#password');
+const pwformconfirm = document.querySelector('#password-confirm');
+const wrongPassword = document.querySelector('.wrong-password');
+
+pwform.addEventListener('focusout', e => {
+    if (pwformconfirm.value === ""){
+
+    }else{
+        if(pwform.value === pwformconfirm.value){
+            wrongPassword.classList.add('hidden');
+        }else{
+            wrongPassword.classList.remove('hidden');
+        }
+    }
+});
+
+pwformconfirm.addEventListener('focusout', e => {
+    if (pwformconfirm.value === ""){
+
+    }else{
+        if(pwform.value === pwformconfirm.value){
+            wrongPassword.classList.add('hidden');
+        }else{
+            wrongPassword.classList.remove('hidden');
+        }
+    }
+})
